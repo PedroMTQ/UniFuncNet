4ac2ac8e9d7bd3d71605afa815fcf3421d8a353e
Pedro Q <pdqueiros@gmail.com>
2021-10-13 13:28:01 +0200

    collecitng kegg_ko when scraping reactions from kegg

--- source/Fetchers/Reaction_Fetchers/Reaction_Fetcher_KEGG.py
+++ source/Fetchers/Reaction_Fetchers/Reaction_Fetcher_KEGG.py
@@ -69,21 +69,26 @@ class Reaction_Fetcher_KEGG(Reaction_Fetcher):
     # End goal function for kegg
     def get_reactions_KEGG(self):
 
-        rn_info, rn_soup = self.reaction_info_KEGG(self.reaction_id)
-        if self.check_correct_api_kegg_search(rn_info['rn_comp_id'], self.convergence_args['compound']):
-            enzyme_ec = self.rn_enzyme(rn_soup)
-            if enzyme_ec:
-                self.convergence_args['proteins_list'] = [i for i in enzyme_ec.split() if not i.endswith('-')]
-            res = {
-                'kegg': self.reaction_id,
-                'reaction_str': rn_info['rn'],
-                'pathways':  rn_info['pathways'],
-            }
-            if rn_info['rn_instances']: res['reaction_with_instances'] = rn_info['rn_instances']
-            else:                       res['rn_with_ids'] = [rn_info['rn'], rn_info['rn_comp_id'], 'kegg']
-            return Reaction(res)
-        else:
-            print('KEGG API returned a reaction from another compound! Reaction not added')
+        rn_info_rn_soup = self.reaction_info_KEGG(self.reaction_id)
+        if rn_info_rn_soup:
+            rn_info, rn_soup= rn_info_rn_soup
+            if self.check_correct_api_kegg_search(rn_info['rn_comp_id'], self.convergence_args['compound']):
+                enzyme_ec = self.rn_enzyme(rn_soup)
+                if enzyme_ec:
+                    self.convergence_args['proteins_list'] = [i for i in enzyme_ec.split() if not i.endswith('-')]
+                orthology = self.textbox_KEGG(rn_soup, 'Orthology', to_split=True)
+                if orthology: orthology = [i.split(' : ')[0] for i in orthology]
+                res = {
+                    'kegg': self.reaction_id,
+                    'reaction_str': rn_info['rn'],
+                    'pathways':  rn_info['pathways'],
+                    'kegg_ko':  orthology,
+                }
+                if rn_info['rn_instances']: res['reaction_with_instances'] = rn_info['rn_instances']
+                else:                       res['rn_with_ids'] = [rn_info['rn'], rn_info['rn_comp_id'], 'kegg']
+                return Reaction(res)
+            else:
+                print('KEGG API returned a reaction from another compound! Reaction not added')
 
     def converge_reaction_global(self):
         self.converge_reaction_to_protein()
